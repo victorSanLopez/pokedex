daf2ec23307cebc79efc72e802b02a4d83f1fc52
Sanesthesie <victorsanlopez@gmail.com>
2024-10-22 22:00:57 +0200

    first commit for branch props.1 : Change code to use props

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,22 @@ import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+	const pokemonList = [
+		{
+			name: "bulbasaur",
+
+			imgSrc:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+		},
+
+		{
+			name: "mew",
+		},
+	];
 	return (
 		<section>
 			<h1>
-				<PokemonCard />
+				<PokemonCard pokemon={pokemonList[0]} />
 			</h1>
 		</section>
 	);
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,4 @@
-function PokemonCard() {
-	const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
 	return (
 		<figure>
 			{pokemon.imgSrc !== undefined ? (
@@ -13,18 +12,5 @@ function PokemonCard() {
 		</figure>
 	);
 }
-
-const pokemonList = [
-	{
-		name: "bulbasaur",
-
-		imgSrc:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-	},
-
-	{
-		name: "mew",
-	},
-];
 
 export default PokemonCard;
