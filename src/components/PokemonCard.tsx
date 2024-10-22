1f6c0497d178d317bb1db8ef5de2e203d6a72673
Sanesthesie <victorsanlopez@gmail.com>
2024-10-22 17:31:28 +0200

    first commit for branch compenents.2 : add a pokemon list and new features

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,30 @@
 function PokemonCard() {
+	const pokemon = pokemonList[0];
 	return (
 		<figure>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-				alt="Bulbasaur"
-			/>
+			{pokemon.imgSrc !== undefined ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>"???"</p>
+			)}
 			<figcaption>
-				<p>"Bulbasaur"</p>
+				<p>{pokemon.name}</p>
 			</figcaption>
 		</figure>
 	);
 }
+
+const pokemonList = [
+	{
+		name: "bulbasaur",
+
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+
+	{
+		name: "mew",
+	},
+];
 
 export default PokemonCard;
