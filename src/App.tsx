124bd03ace06ffa069797d79966ec1794f350bf8
Sanesthesie <victorsanlopez@gmail.com>
2024-10-24 11:45:32 +0200

    first commit on branch props.2 : add buttons next and previous on Pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,77 @@
 import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 function App() {
-	const pokemonList = [
-		{
-			name: "bulbasaur",
+  const pokemonList = [
+    {
+      name: "bulbasaur",
 
-			imgSrc:
-				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-		},
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
 
-		{
-			name: "mew",
-		},
-	];
-	return (
-		<section>
-			<h1>
-				<PokemonCard
-					imgSrc={pokemonList[0].imgSrc}
-					name={pokemonList[0].name}
-				/>
-			</h1>
-		</section>
-	);
+    {
+      name: "charmander",
+
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+
+    {
+      name: "squirtle",
+
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+
+    {
+      name: "pikachu",
+
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+
+    {
+      name: "mew",
+    },
+  ];
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClickNext = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
+  const handleClickPrevious = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
+  return (
+    <section>
+      <h1>
+        <PokemonCard
+          imgSrc={pokemonList[pokemonIndex].imgSrc}
+          name={pokemonList[pokemonIndex].name}
+        />
+      </h1>
+      {pokemonIndex > 0 ? (
+        <button type="button" onClick={handleClickPrevious}>
+          Précédent
+        </button>
+      ) : (
+        <></>
+      )}
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button type="button" onClick={handleClickNext}>
+          Suivant
+        </button>
+      ) : (
+        <></>
+      )}
+    </section>
+  );
 }
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,17 +1,17 @@
-interface pokemon {
-	name: string;
-	imgSrc?: string;
-}
+type pokemon = {
+  name: string;
+  imgSrc?: string;
+};
 
 function PokemonCard({ imgSrc, name }: pokemon) {
-	return (
-		<figure>
-			{imgSrc !== undefined ? <img src={imgSrc} alt={name} /> : <p>"???"</p>}
-			<figcaption>
-				<p>{name}</p>
-			</figcaption>
-		</figure>
-	);
+  return (
+    <figure>
+      {imgSrc !== undefined ? <img src={imgSrc} alt={name} /> : <p>"???"</p>}
+      <figcaption>
+        <p>{name}</p>
+      </figcaption>
+    </figure>
+  );
 }
 
 export default PokemonCard;
